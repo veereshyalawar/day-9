b12af5dd37ae76fb23388953b596502dd50335d8
veeresh <veereshyalawar2@gmail.com>
2022-02-22 08:24:14 -0800

    added uc3 file

---
+++ uc_3.java
@@ -0,0 +1,23 @@
+
+public class uc_3 {
+
+	public static void main(String[] args) {
+		int numOfWorkingDays = 20;
+		int totalEmpHr = 0;
+		int totalWorkingDays = 0;
+		int empHrs = 0;
+
+		double randomCheck = Math.floor(Math.random() * 10) % 2;
+		int i = (int) randomCheck;
+	        if(i = Is_Full_Time)
+                {
+		System.out.println("present");
+		}
+		else 
+		{
+
+			System.out.println("absent");
+		}
+		}
+}
+
