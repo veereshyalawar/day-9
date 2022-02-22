851262fd25ffd263d0830f744db6dfe74a9058c4
veeresh <veereshyalawar2@gmail.com>
2022-02-22 08:20:16 -0800

    added uc1 file

---
+++ uc_1.java
@@ -0,0 +1,15 @@
+public class uc1 {
+
+	public static void main(String[] args) {
+
+		int Is_Full_Time = 1;
+
+		double empCheck = Math.floor(Math.random() * 10) % 2;
+
+		if (empCheck == Is_Full_Time)
+			System.out.println("employ is present");
+		else
+			System.out.println("employ is absent");
+	}
+
+}
